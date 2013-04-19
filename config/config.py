d78fc807ec9c41c0d5ef12df55ff98f1a322796c
dustin <dstywho@gmail.com>
2013-04-19 14:00:19 -0400

    pep8 formatting

--- config/config.py
+++ config/config.py
@@ -1,22 +1,17 @@
 from factories.param_factory import ParamFactory
-from ovirtsdk.xml import params
-from ovirtsdk.infrastructure.errors import RequestError
-from ovirtsdk.api import API
 import jsonpickle
 
 
-
 class Item(object):
-    def __init__(self, args, extras={},creation=None):
+    def __init__(self, args, extras={}, creation=None):
         self.args = args
         self.extras = extras
-        self.creation=creation
-
+        self.creation = creation
 
     def transform_extras(self):
         result = {}
         for key in self.extras:
-            result[key] = self.extras[key].create() 
+            result[key] = self.extras[key].create()
         return result
 
     def compiled_args(self):
@@ -29,66 +24,67 @@ class Item(object):
         return self.creation(**compiled_args())
 
 
-
 class Version(Item):
-    def __init__(self,args,extras={}):
-        Item.__init__(self,args,extras )
+    def __init__(self, args, extras={}):
+        Item.__init__(self, args, extras)
 
     def create(self):
         return ParamFactory().create_version(**self.compiled_args())
 
 
 class Datacenter(Item):
-    def __init__(self,args,extras={}):
-        Item.__init__(self,args,extras )
+    def __init__(self, args, extras={}):
+        Item.__init__(self, args, extras)
 
     def create(self):
         return ParamFactory().create_datacenter(**self.compiled_args())
 
 
 class Cluster(Item):
-    def __init__(self,args,extras={}):
-        Item.__init__(self,args,extras )
+    def __init__(self, args, extras={}):
+        Item.__init__(self, args, extras)
 
     def create(self):
         return ParamFactory().create_cluster(**self.compiled_args())
 
+
 class CPU(Item):
-    def __init__(self,args,extras={}):
-        Item.__init__(self,args,extras )
+    def __init__(self, args, extras={}):
+        Item.__init__(self, args, extras)
 
     def create(self):
         return ParamFactory().create_cpu(**self.compiled_args())
 
+
 class Host(Item):
-    def __init__(self,args,extras={}):
-        Item.__init__(self,args,extras )
+    def __init__(self, args, extras={}):
+        Item.__init__(self, args, extras)
 
     def create(self):
         return ParamFactory().create_host(**self.compiled_args())
 
 
-
 class Config:
     def __init__(self):
-        self.rest_api= {'url':'https://localhost:443/api', 'credentials': {'username':'admin@internal', 'password':'redhat'}}
-        self.version = Version({'major':'3', 'minor':'1'})
-        self.datacenter = Datacenter({'name':'mydatacenter', 'description':'a description', 'storage_type':'posixfs'} ,{'version':self.version})
-        cpu = CPU({'id':'Intel SandyBridge Family'})
-        self.cluster = Cluster({'name':"mycluster", 'virt_service':False, 'gluster_service':True} , { 'datacenter':self.datacenter , 'version':self.version, 'cpu': cpu })
+        self.rest_api = {'url': 'https://localhost:443/api', 'credentials':
+                         {'username': 'admin@internal', 'password': 'redhat'}}
+        self.version = Version({'major': '3', 'minor': '1'})
+        self.datacenter = Datacenter({'name': 'mydatacenter',
+                                      'description': 'a description', 'storage_type': 'posixfs'}, {'version': self.version})
+        cpu = CPU({'id': 'Intel SandyBridge Family'})
+        self.cluster = Cluster({'name': "mycluster", 'virt_service': False, 'gluster_service': True}, {'datacenter': self.datacenter, 'version': self.version, 'cpu':  cpu})
         self.hosts = []
-        self.hosts.append( Host({'name':'myhost', 'host':'rhevm-sf101-node-a', 'root_password':"redhat"}, {'cluster':self.cluster}))
-        self.hosts.append( Host({'name':'myhost2', 'host':'rhevm-sf101-node-a', 'root_password':"redhat"}, {'cluster':self.cluster}))
-
+        self.hosts.append(Host({'name': 'myhost', 'host': 'rhevm-sf101-node-a', 'root_password': "redhat"}, {'cluster': self.cluster}))
+        self.hosts.append(Host({'name': 'myhost2', 'host': 'rhevm-sf101-node-a', 'root_password': "redhat"}, {'cluster': self.cluster}))
 
     def to_json(self):
-      return jsonpickle.encode(self)
+        return jsonpickle.encode(self)
 
-    def get_host_by_name(self,name):
-        return filter(lambda x: x.compiled_args()['name']== name , self.hosts)[0].create()
+    def get_host_by_name(self, name):
+        return filter(lambda x: x.compiled_args()['name'] == name, self.hosts)[0].create()
 
     @classmethod
-    def from_json(cls,json):
+    def from_json(cls, json):
         return jsonpickle.decode(json)
 
     @classmethod
@@ -98,18 +94,17 @@ class Config:
 
 
 if __name__ == "__main__":
-
-    json =  Config().to_json()
+    json = Config().to_json()
     print json
 #    #print Config().from_json(json).datacenter.create()
-#    import pdb; pdb.set_trace()    
+#    import pdb; pdb.set_trace()
 #    print Config().from_json(json).hosts[0].create().get_cluster().get_data_center().get_name()
 #    print Config().from_json(json).hosts[0].create().get_cluster().get_data_center().get_name()
 
-    
+
 
 
     #json = open('config/config.json', 'r').read()
     #config = Config.from_json(json)
     #myhost = config.get_host_by_name('node1')
-    #print myhost.get_name() 
+    #print myhost.get_name()
